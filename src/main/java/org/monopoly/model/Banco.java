14afdfdb87abf6012bb070e4f238e7e58cceb2a1
MatiBartellone <matias.bartellone@gmail.com>
2024-03-31 21:54:30 -0300

    agrego Banco v1.0

--- src/main/java/org/monopoly/model/Banco.java
+++ src/main/java/org/monopoly/model/Banco.java
@@ -1,4 +1,22 @@
 package org.monopoly.model;
 
 public class Banco {
+    private int dinero;
+
+    public Banco(int dineroTotal){
+        this.dinero = dineroTotal;
+    }
+
+    public void otorgarDinero(Jugador jugador, int monto){
+        jugador.sumarDinero(monto);
+        dinero -= monto;
+    }
+    public boolean recibirDinero(Jugador jugador, int monto){
+        if (jugador.getDinero() < monto){
+            return false;
+        }
+        jugador.retirarDinero(monto);
+        dinero += monto;
+        return true;
+    }
 }
