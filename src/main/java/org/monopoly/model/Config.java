c4d3a217e37ce99e88e6d719ca664a7e22aa42ea
MatiBartellone <matias.bartellone@gmail.com>
2024-03-31 22:41:48 -0300

    agrego Config v1.0

--- src/main/java/org/monopoly/model/Config.java
+++ src/main/java/org/monopoly/model/Config.java
@@ -1,4 +1,20 @@
 package org.monopoly.model;
 
+import org.monopoly.model.casilla.Casilla;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Config {
+    public static enum ColoresJugadores{AUTO, SOMBRERO, PLANCHA, BARCO}
+    public static enum EstadosJugadores{EN_JUEGO, PRESO, QUEBRADO}
+    public static enum TiposCasillas{PROPIEDAD, ESTACION, IR_A_CARCEL, MULTA, LOTERIA, CARCEL, SALIDA, PASO}
+    public static enum ColoresPropiedades{FERROCARRIL, MARRON, CELESTE, ROSA, NARANJA, ROJO, AMARILLO, VERDE, AZUL}
+    public static int DineroInicial = 1500;
+    public static List<Casilla> ListaCasillas = new ArrayList<Casilla>(){{
+        add(new Casilla());
+        add(new Casilla());
+        add(new Casilla());
+        add(new Casilla());
+    }};
 }
