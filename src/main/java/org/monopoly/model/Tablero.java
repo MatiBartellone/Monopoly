44afe0585dfd6734cedfb1f5091d3b10a07c8500
thiagopservian <tpacheco@fi.uba.ar>
2024-04-14 01:23:05 -0300

    tablero 1.0

--- src/main/java/org/monopoly/model/Tablero.java
+++ src/main/java/org/monopoly/model/Tablero.java
@@ -1,4 +1,25 @@
 package org.monopoly.model;
+import org.monopoly.model.Config;
+import org.monopoly.model.casilla.Casilla;
+
+import java.util.List;
 
 public class Tablero {
+    private List<Casilla> listaCasillas;
+
+    public Tablero(){
+        this.listaCasillas = Config.ListaCasillas;
+    }
+
+    public List<Casilla> getListaCasillas(){
+        return this.listaCasillas;
+    }
+
+    public Casilla casillaSiguiente(Casilla casillaActual, int avances){
+        int index = this.listaCasillas.indexOf(casillaActual) + avances;
+        if (index>=this.listaCasillas.size()){
+            index = index - this.listaCasillas.size();
+        }
+        return this.listaCasillas.get(index);
+    }
 }
