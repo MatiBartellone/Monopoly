c6c75ece239df66ea062aa8c806fd0001371759c
thiagopservian <tpacheco@fi.uba.ar>
2024-04-16 23:06:45 -0300

    correccion del constructor

--- src/main/java/org/monopoly/model/Tablero.java
+++ src/main/java/org/monopoly/model/Tablero.java
@@ -1,5 +1,4 @@
 package org.monopoly.model;
-import org.monopoly.model.Config;
 import org.monopoly.model.casilla.Casilla;
 
 import java.util.List;
@@ -7,8 +6,8 @@ import java.util.List;
 public class Tablero {
     private List<Casilla> listaCasillas;
 
-    public Tablero(){
-        this.listaCasillas = Config.ListaCasillas;
+    public Tablero(List<Casilla> lista){
+        this.listaCasillas = lista;
     }
 
     public List<Casilla> getListaCasillas(){
