36042ca4cb22790ca531ae7b16dbac0ddeaa5d49
thiagopservian <tpacheco@fi.uba.ar>
2024-04-25 14:30:06 -0300

    agrego construiible

---
+++ src/main/java/org/monopoly/model/casilla/Construible.java
@@ -0,0 +1,9 @@
+package org.monopoly.model.casilla;
+
+public interface Construible {
+    public void construir();
+    public void destruir();
+    public int getCantConstruidos();
+    public int getValorConstruir();
+    public int getValorDestruir();
+}
