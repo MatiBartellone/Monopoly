669caf716f110eb43c7457b5a197fa925fbded9c
thiagopservian <tpacheco@fi.uba.ar>
2024-04-24 23:40:59 -0300

    commit de resguardo

---
+++ src/main/java/org/monopoly/controller/validador/CalculadoraDeAcciones.java
@@ -0,0 +1,13 @@
+package org.monopoly.controller.validador;
+
+import org.monopoly.model.RegistroComprables;
+import org.monopoly.model.Jugador;
+import org.monopoly.model.Juego;
+
+public class CalculadoraDeAcciones implements Validador{
+    private Juego juego;
+    private RegistroComprables registro;
+
+
+
+}
