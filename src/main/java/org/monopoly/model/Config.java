37c41b9438dfdf1eeb7088bc9a85f1f3d6e54911
ivan-maximoff <maximoff.ivan@gmail.com>
2024-04-25 23:24:30 -0300

    config

--- src/main/java/org/monopoly/model/Config.java
+++ src/main/java/org/monopoly/model/Config.java
@@ -33,21 +33,27 @@ public class Config {
     public static CasillaSinEfecto carcel = new CasillaSinEfecto(TiposCasillas.CARCEL);
     public static List<Propiedad> ListaPropiedades = new ArrayList<Propiedad>(){{
         add(new Propiedad(TiposCasillas.PROPIEDAD, "Mediterraneo", 60, 4, ColoresComprables.brown,  construcciones, 50));
-
-
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Baltco", 60, 6, ColoresComprables.brown,  construcciones, 50));
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Central", 100, 10, ColoresComprables.lightblue,  construcciones, 50));
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Vermont", 100, 12, ColoresComprables.lightblue, construcciones, 50));
         add(new Propiedad(TiposCasillas.PROPIEDAD, "Connecticut", 120, 16, ColoresComprables.lightblue, construcciones, 50));
         add(new Propiedad(TiposCasillas.PROPIEDAD, "St.Charles", 140, 20, ColoresComprables.pink, construcciones, 100));
-
-
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "States", 140, 22, ColoresComprables.pink, construcciones, 150));
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Virginia", 160, 28, ColoresComprables.pink, construcciones, 150));
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "St.James", 180, 30, ColoresComprables.orange, construcciones, 150));
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Tennesse", 180, 32, ColoresComprables.orange, construcciones, 150));
         add(new Propiedad(TiposCasillas.PROPIEDAD, "New York", 200, 36, ColoresComprables.orange, construcciones, 150));
         add(new Propiedad(TiposCasillas.PROPIEDAD, "Kentucky", 220, 38, ColoresComprables.red, construcciones, 150));
-
-
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Indiana", 220, 40, ColoresComprables.red, construcciones, 150));
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Illinois", 240, 44, ColoresComprables.red, construcciones, 150));
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Atlantic", 260, 46, ColoresComprables.yellow, construcciones, 150));
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Ventor", 260, 48, ColoresComprables.yellow, construcciones, 150));
         add(new Propiedad(TiposCasillas.PROPIEDAD, "Marvin", 280, 52, ColoresComprables.yellow, construcciones, 150));
-
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Pacific", 300, 54, ColoresComprables.green, construcciones, 200));
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "North Carolina", 300, 56, ColoresComprables.green, construcciones, 200));
         add(new Propiedad(TiposCasillas.PROPIEDAD, "Pennsylvania", 320, 60, ColoresComprables.green, construcciones, 200));
         add(new Propiedad(TiposCasillas.PROPIEDAD, "Park Place", 350, 65, ColoresComprables.blue, construcciones, 200));
-
+        add(new Propiedad(TiposCasillas.PROPIEDAD, "Boardwalk", 400, 70, ColoresComprables.blue, construcciones, 200));
     }};
 
     public static List<Estacion> ListaEstaciones= new ArrayList<Estacion>(){{
@@ -67,20 +73,37 @@ public class Config {
          List<Casilla> listaCasillas = new ArrayList<Casilla>();
          listaCasillas.add(new CasillaSinEfecto(TiposCasillas.SALIDA));
          AtomicInteger indice = new AtomicInteger(0);
-        listaCasillas.add(new Multa(TiposCasillas.MULTA, 1000));
-        listaCasillas.add(new Multa(TiposCasillas.MULTA, 1000));
-        listaCasillas.add(new Multa(TiposCasillas.MULTA, 1000));
-         agregarPropiedad(listaCasillas, indice, 3);
-        listaCasillas.add(new Multa(TiposCasillas.MULTA, 1000));
-        listaCasillas.add(ListaEstaciones.get(0));
+         agregarPropiedad(listaCasillas, indice, 2);
+         listaCasillas.add(new Multa(TiposCasillas.MULTA, 200));
+         listaCasillas.add(ListaEstaciones.get(0));
+         agregarPropiedad(listaCasillas, indice, 2);
+         listaCasillas.add(new Loteria(TiposCasillas.LOTERIA, 200));
+         agregarPropiedad(listaCasillas, indice, 1);
+
          listaCasillas.add(carcel);
 
-        agregarPropiedad(listaCasillas, indice, 3);
-        listaCasillas.add(new Multa(TiposCasillas.MULTA, 1000));
-        agregarPropiedad(listaCasillas, indice, 2);
-        listaCasillas.add(new Multa(TiposCasillas.MULTA, 1000));
-        listaCasillas.add(new Multa(TiposCasillas.MULTA, 1000));
+         agregarPropiedad(listaCasillas, indice, 3);
+         listaCasillas.add(ListaEstaciones.get(1));
+         agregarPropiedad(listaCasillas, indice, 1);
+         listaCasillas.add(new Multa(TiposCasillas.MULTA, 200));
+         agregarPropiedad(listaCasillas, indice, 2);
+         listaCasillas.add(new CasillaSinEfecto(TiposCasillas.PASO));
 
+         agregarPropiedad(listaCasillas, indice, 1);
+         listaCasillas.add(new Multa(TiposCasillas.MULTA, 200));
+         agregarPropiedad(listaCasillas, indice, 2);
+         listaCasillas.add(ListaEstaciones.get(2));
+         agregarPropiedad(listaCasillas, indice, 3);
+
+         listaCasillas.add(new IrACarcel(TiposCasillas.IR_A_CARCEL, carcel));
+
+         agregarPropiedad(listaCasillas, indice, 2);
+         listaCasillas.add(new Loteria(TiposCasillas.LOTERIA, 200));
+         agregarPropiedad(listaCasillas, indice, 1);
+         listaCasillas.add(ListaEstaciones.get(3));
+         agregarPropiedad(listaCasillas, indice, 1);
+         listaCasillas.add(new Multa(TiposCasillas.MULTA, 200));
+         agregarPropiedad(listaCasillas, indice, 1);
 
          return listaCasillas;
     }
